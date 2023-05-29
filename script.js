a9d0435efc48910835c5e3760a9439a097e9b30d
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-05-29 18:29:20 +0200

    adding functions

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const temp = document.getElementById("temp")
 const date = document.getElementById("date-time")
+const currentLocation = document.getElementById("location")
+const condition = document.getElementById("condition")
+const rain = document.getElementById("rain")
+const mainIcon = document.getElementById("icon")
+
+const windSpeed = document.getElementById('wind-speed')
+
+const sunRise = document.querySelector("sunRise")
+const sunSet = document.querySelector("sunSet")
+
+const humidity = document.querySelector("humidity")
+const humidityStatus = document.querySelector("humidityStatus")
+
+const visibility = document.querySelector("visibility")
+const visibilityStatus = document.querySelector("visibilityStatus")
+
+const airQuality = document.querySelector("airQuality")
+const airQualityStatus = document.querySelector("airQualityStatus")
+
 
 
 let currentCity = '';
@@ -59,8 +78,22 @@ getPublicIp();
 
 
 function getWeatherData(city, unit, hourlyOrWeekly) {
-    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/world?unitGroup=metric&key=${apiKey}&contentType=json")
-
+    const apiKey = "JCELDRLJGXLJY6S9AJP4QUP94"
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?elements=datetime%2CdatetimeEpoch%2Ctemp%2Ctempmax%2Ctempmin%2Cprecip%2Cwindspeed%2Cwindgust%2Cfeelslike%2Cfeelslikemax%2Cfeelslikemin%2Cpressure%2Cstations%2Cdegreedays%2Caccdegreedays&include=fcst%2Cobs%2Chistfcst%2Cstats%2Chours&key=${apiKey}&options=preview&contentType=json`)
+    .then((response)=> response.json())
+    .then((data)=>{
+        let today = data.currentConditions;
+        if(unit === "c"){
+            temp.innerText = today.temp
+        }else{
+            temp.innerText = celciusToFahrenheit(today.temp)
+        }
+        currentLocation.innerText = data.resolvedAddress
+        CSSConditionRule.innerText = today.conditions
+    })
 }
 
-getWeatherData()
+
+function celciusToFahrenheit(temp){
+    return((temp*9)/5 +32).toFixed(1)
+}
